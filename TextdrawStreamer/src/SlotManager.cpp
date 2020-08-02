b12c73b6e9fca64bb0629549bd6b26147537c4ec
Burak <pro.daggermen@gmail.com>
2020-08-02 10:47:48 +0300

    Update SlotManager.cpp

--- TextdrawStreamer/src/SlotManager.cpp
+++ TextdrawStreamer/src/SlotManager.cpp
@@ -16,55 +16,40 @@
 
 #include "SlotManager.h"
 
-std::map<int, std::vector<int>> SlotManager::Slot;
+std::map<int, std::priority_queue<size_t, std::vector<size_t>, std::greater<size_t>>> SlotManager::Slot;
+std::map<int, size_t> SlotManager::Slot_Counter;
 
 size_t SlotManager::Free_ID(size_t playerid)
 {
-	if (Slot[playerid].empty()) return 1;
-
-	std::sort(Slot[playerid].begin(), Slot[playerid].end());
-
-	size_t sira = 1;
-
-	for (std::vector<int>::iterator it = Slot[playerid].begin(); it != Slot[playerid].end(); ++it)
+	int id = Start_ID;
+	if (Slot[playerid].empty())
 	{
-		if (*it != sira) return sira;
-		++sira;
+		id = Slot_Counter[playerid]++;
 	}
-	return sira;
-}
-
-void SlotManager::Add_ID(size_t playerid, size_t id)
-{
-	Slot[playerid].push_back(id);
+	else
+	{
+		id = Slot[playerid].top();
+		Slot[playerid].pop();
+	}
+	return id;
 }
 
 void SlotManager::Remove_ID(size_t playerid, size_t id)
 {
-	auto sonuc = std::find(Slot[playerid].begin(), Slot[playerid].end(), id);
-	if (sonuc != Slot[playerid].end())
-	{
-		if (LOG_MODE)
-		{
-			sampgdk::logprintf("%s ID[%d]: %d kaldirildi.", LOG, playerid, id);
-		}
-		Slot[playerid].erase(sonuc);
-	}
+	Slot[playerid].push(id);
 }
 
 void SlotManager::Reset_ID(size_t playerid)
 {
-	if (LOG_MODE)
-	{
-		sampgdk::logprintf("%s ID: %d resetlendi.", LOG, playerid);
-	}
-	Slot[playerid].clear();
+	Slot_Counter[playerid] = Start_ID;
+	Slot[playerid] = std::priority_queue<size_t, std::vector<size_t>, std::greater<size_t>>();
 }
 
 void SlotManager::Reset_All_ID()
 {
 	for (size_t i = 0; i < MAX_PLAYERS; i++)
 	{
-		Slot[i].clear();
+		Slot_Counter[i] = Start_ID;
+		Slot[i] = std::priority_queue<size_t, std::vector<size_t>, std::greater<size_t>>();
 	}
 }
